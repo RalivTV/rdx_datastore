b854ab9df5933bdd1cd66173bb86f503c8310ca0
RalivTV <58900773+RalivTV@users.noreply.github.com>
2020-07-05 22:02:48 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'ESX Data Store'
+
+version '1.0.2'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/classes/datastore.lua',
+	'server/main.lua'
+}
